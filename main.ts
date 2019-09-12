869d5bf95d376fe1562e95ebf9fe712f71f32ff1
Shozo KUZE <34113514+mathrax-s@users.noreply.github.com>
2019-09-12 17:39:34 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=70 icon="\uf1db" color=#EC7505
 namespace comment {
-    //% blockId=show_strings block="コメント %v"
+    //% blockId=tropic_pick block="コメント %v"
     export function noaction(text: string): void {
     }
 }
